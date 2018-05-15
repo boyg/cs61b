ec13b9b752fddb9e480e13679f63a27fc7168d29
boyg <st.farra@gmai.com>
2018-05-14 17:28:22 -0700

    Implemented SLLList methods

---
+++ lec1/SLList.java
@@ -0,0 +1,45 @@
+public class SLList {
+    public static class IntNode {
+        public int item;
+        public IntNode next;
+        public IntNode(int i, IntNode n) {
+            item = i;
+            next = n;
+        }
+    }
+    
+    private IntNode first;
+
+    public SLList(int x) {
+        first = new IntNode(x, null);
+    }
+
+    public void addFirst(int x) {
+        first = new IntNode(x, first)
+    }
+
+    public int getFirst() {
+        return first.item;
+    }
+
+    public void addLast(int x) {
+        IntNode p = first;
+
+        while (p.next != null) {
+            p = p.next;
+        }
+        p.next = new IntNode(x, null);
+    }
+
+    public int size(IntNode p) {
+        if (first == null) {
+            return 0;
+        } else {
+            return 1 + size(p.next);
+        }
+    }
+
+    public int size() {
+        return size(first);
+    }
+}
