dcaed87f4e58b5dbafdef796cdaa5a47fa240e2f
boyg <st.farra@gmai.com>
2018-06-05 23:14:16 -0700

    halfway finished LinkedListDeque, no testing done

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,30 +1,88 @@
-public class LinkedListDeque<DataType> {
-    public class IntNode {
-        public IntNode prev;
-        public DataType item;
-        public IntNode next;
-        public IntNode(IntNode p, DataType i, IntNode n) {
+public class LinkedListDeque<T> {
+    public static class Node {
+        public Node prev;
+        public T item;
+        public Node next;
+        public Node(Node p, T i, Node n) {
             prev = p;
             item = i;
             next = n;
         }
     }
 
-    private IntNode sentinel;
+    private Node sentinel;
     private int size;
 
     public LinkedListDeque() {
-        sentinel = new IntNode(this, -1, this);
+        sentinel = new Node(this, 0, this);
         size = 0;
     }
 
+    public boolean isEmpty() {
+        return (sentinel.next == this);
+    }
+
     public void addFirst(T item) {
-        if (sentinel.next == this) {
-            sentinel.next = new IntNode(sentinel, item, sentinel);
+        if (isEmpty()) {
+            sentinel.next = new Node(sentinel, item, sentinel);
             sentinel.prev = sentinel.next;
         } else {
-            IntNode p = sentinel.next;
-            // continue here
+            Node p = sentinel.next;
+            sentinel.next = new Node(sentinel, item, p);
+            p.prev = sentinel.next;
         }
+        size++;
+    }
+
+    public void addLast(T item) {
+        if (isEmpty()) {
+            sentinel.next = new Node(sentinel, item, sentinel);
+            sentinel.prev = sentinel.next;
+        } else {
+            Node p = sentinel.prev;
+            sentinel.prev = new Node(p, item, sentinel);
+            p.next = sentinel.prev;
+        }
+        size++;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        Node p = sentinel;
+        while (p.next != null) {
+            p = p.next;
+            System.out.print(p.item + " ");
+        }
+    }
+
+    public T removeFirst() {
+        if (isEmpty()) {
+            return null;
+        } else {
+            // TODO: finish removeFirst
+        }
+    }
+
+    public T removeLast() {
+        if (isEmpty()) {
+            return null;
+        } else {
+            // TODO: finish removeLast
+        }
+    }
+
+    public T get(int index) {
+        if (index < 0 || index > size - 1) {
+            return null
+        } else {
+            // TODO: finish get
+        }
+    }
+
+    public T getRecursive(int index) {
+        // TODO: finish getRecursive
     }
 }
